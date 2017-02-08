de31e3515675e30880254b7478cfdbcfc5ab2331
CornyElephant <eldraco@mac.com>
2017-02-08 14:38:59 -0500

    Add catapult with animations and controller, as well as make tweaks to materials and script parameters for snipertower.

---
+++ Assets/CatapultFire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultFire : MonoBehaviour {
+
+	public GameObject boulder;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (FireLoop ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FireLoop(){
+		while (true) {
+			Animator animation = GetComponent<Animator> ();
+
+			animation.SetTrigger ("Fire");
+			yield return new WaitForSeconds (0.5f / animation.GetFloat ("FireSpeed"));
+			boulder.SetActive (false);
+			yield return new WaitForSeconds (0.5f / animation.GetFloat ("FireSpeed"));
+			yield return new WaitForSeconds (1.0f / animation.GetFloat("ReloadSpeed"));
+			boulder.SetActive (true);
+		}
+	}
+}
